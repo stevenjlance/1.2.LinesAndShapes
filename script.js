3770aed7b64ecc27dcaa10857c801aa181111af8
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-01-28 14:56:42 +0000

    Solutions added

--- script.js
+++ script.js
@@ -15,21 +15,28 @@ function draw() {
 
 
 	// Practice 1
-	
+	rect(0, 0, 50, 50)
+	rect(350, 0, 50, 50)
+	rect(0, 350, 50, 50)
+	rect(350, 350, 50, 50)
 	
 
 
 	// Practice 2
-	
-	
-
+	// Same as above, plus sthe code below
+	rect(150, 150, 50, 50)
+	rect(200, 150, 50, 50)
+	rect(150, 200, 50, 50)
+	rect(200, 200, 50, 50)
 
 	// Practice 3
+	// Code above plus the following
+	ellipse(25, 25, 50, 50)
+	ellipse(375, 25, 50, 50)
+	ellipse(25, 375, 50, 50)
+	ellipse(375, 375, 50, 50)
 
 	
-
-	// Practice 4
-
 	
 
 
